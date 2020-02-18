61ca169687d7f563ac9f27df865795a530ac78e4
AndreyKryzhniov <andreykryzhniov@gmail.com>
2020-02-18 19:22:40 +0300

    Updates

---
+++ precache-manifest.e2ac184426bcc1e962fc21357a9ec1e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "464fefa147d9adb85029698fea83f586",
+    "url": "/my_cv/index.html"
+  },
+  {
+    "revision": "717cc20fc1fcdf4eee3c",
+    "url": "/my_cv/static/css/main.9805951b.chunk.css"
+  },
+  {
+    "revision": "d1fb40a6ff31ea3833fc",
+    "url": "/my_cv/static/js/2.da4fcc97.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my_cv/static/js/2.da4fcc97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "717cc20fc1fcdf4eee3c",
+    "url": "/my_cv/static/js/main.fecc670d.chunk.js"
+  },
+  {
+    "revision": "e5321c6d89643dbb7326",
+    "url": "/my_cv/static/js/runtime-main.ab0399f3.js"
+  },
+  {
+    "revision": "b6269be20f5742750ab6acdde3db1658",
+    "url": "/my_cv/static/media/Avatar.b6269be2.jpg"
+  },
+  {
+    "revision": "16fb6184a3801153d0d22ac8a22f0dc9",
+    "url": "/my_cv/static/media/header4.16fb6184.png"
+  }
+]);
